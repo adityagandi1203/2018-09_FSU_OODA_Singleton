fc0dedfc6902bfe1f9042218d00a813887a3135a
adityagandi1203 <43256465+adityagandi1203@users.noreply.github.com>
2018-12-03 14:24:00 -0500

    Update SubClassSingleton.java

--- src/HW9/edu/fsu/csc7400/patterns/singleton/SubClassSingleton.java
+++ src/HW9/edu/fsu/csc7400/patterns/singleton/SubClassSingleton.java
@@ -2,7 +2,7 @@
  * Class: Object-Oriented Design and Analysis
  * Professor: Orlando Montalvo
  * Assignment: HW 9 - Singleton Problems
- * 
+ * Student:Aditya (@01392300)
  * Date: 2017-11-11
  */
 package HW9.edu.fsu.csc7400.patterns.singleton;
@@ -16,9 +16,11 @@ public class SubClassSingleton extends BaseSingleton {
 
 	/**
 	 * Simple function that sets state
+	 *
+	*I did the name corrections as the method name should be same as we are extending the base class
 	 */
 	@Override
-	public void DoSomething() {
+	public void doSomething() {
 		setSomeState(2);
 	}
 
@@ -34,7 +36,28 @@ public class SubClassSingleton extends BaseSingleton {
 
 	/**
 	 * Single constructor must be private
+	 * 
+	 * it have a super constructor so this constructor must be made public and changing the super constructor to protected
 	 */
-	private SubClassSingleton() {
+	public SubClassSingleton() 
+	{
 	}
+		/*singleton unique instance*/
+	private static SubClassSingleton uniqueinstance;
+	/*
+	 * this method used to create the unique instance for subclasssingelton if it have no instance
+	 * @return the new instance or return the old
+	 */
+	public static synchronized BaseSingleton getInstance()
+	{
+		if(uniqueinstance==null)
+		
+			uniqueinstance=new SubClassSingleton();
+			
+		
+		return uniqueinstance;
+			
+	}
+		
+	
 }
